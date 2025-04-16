3118a7fd997d3fa4e80d8dc2e2ae40e690f77562
officialSaqib <143649215+officialSaqib@users.noreply.github.com>
2025-04-16 18:34:21 -0400

    Refactored CLI-tool with clear arguments, and help argument

---
+++ Src/WebcamImgProc.CLI/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using Mono.Options;
+
+namespace WebcamImgProc.CLI
+{
+    /// <summary>
+    /// General helpers used by command-line program.
+    /// </summary>
+    internal static class Helpers
+    {
+        /// <summary>
+        /// Prints a help message to the console output.
+        /// </summary>
+        /// <param name="options">The options (acceptable arguments) for the application.</param>
+        public static void PrintHelp(OptionSet options)
+        {
+            string execStr = Path.GetFileNameWithoutExtension(Environment.GetCommandLineArgs()[0]);
+
+            Console.WriteLine($"Usage: {execStr} [Arguments]\r\n\r\nArguments:");
+            options.WriteOptionDescriptions(Console.Out);
+        }
+    }
+}
--- Src/WebcamImgProc.CLI/Program.cs
+++ Src/WebcamImgProc.CLI/Program.cs
@@ -1,62 +1,115 @@
-﻿using System.Drawing;
-using System.Windows.Controls;
+﻿using Mono.Options;
+using WebcamImgProc.CLI;
 using WebcamImgProc.ImgProc.Capture;
 using WebcamImgProc.ImgProc.Frame;
 
-var capture = new Webcam();
-capture.GrabFrame();
+///////////////////////////////////////////////////////////////////////////////
+//
+// Argument Flags
+//
+bool showHelp = false;
+bool outputGreyscale = false;
+bool outputGreyscaleHistogram = false;
+bool outputGaussianBlur = false;
+bool outputCannyEdgeDetection = false;
 
-void TakeWebcamImage()
+///////////////////////////////////////////////////////////////////////////////
+//
+// Filter Settings
+//
+int kSizeX = 5;                          // Gaussian Blur
+int kSizeY = 5;                          // Gaussian Blur
+double sigmaX = 0;                       // Gaussian Blur
+double thresholdLower = 100;             // Canny Edge Detection
+double thresholdUpper = 200;             // Canny Edge Detection
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Arguments
+//
+var options = new OptionSet
 {
-    capture.GrabFrame().GetBitmap().Save("webcam.png");
+    { "h|help", "show this message then exit", arg => showHelp = true },
+    { "g|grey", "output greyscale image", arg => outputGreyscale = true },
+    { "histogram", "output greyscale histogram", arg => outputGreyscaleHistogram = true },
+    { "b|blur", "output image with Gaussian Blur", arg => outputGaussianBlur = true },
+    { "ksize-x=", "kernel x-size for Gaussian Blur (default=5)", arg => kSizeX = int.Parse(arg) },
+    { "ksize-y=", "kernel y-size for Gaussian Blur (default=5)", arg => kSizeY = int.Parse(arg) },
+    { "sigma-x=", "STD in x-axis for Gaussian Blur (default=0.0)", arg => sigmaX = double.Parse(arg) },
+    { "e|edgedetection", "output image with Canny Edge Detection", arg => outputCannyEdgeDetection = true },
+    { "threshold-l=", "lower threshold for Canny Edge Detection (default=100.0)", arg => thresholdLower = double.Parse(arg) },
+    { "threshold-u=", "upper threshold for Canny Edge Detection (default=200.0)", arg => thresholdUpper = double.Parse(arg) },
+};
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Argument Parsing
+//
+try
+{
+    options.Parse(args);
+} catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+    Helpers.PrintHelp(options);
+    return;
 }
-void TakeWebcamImageGreyscale()
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Argument Processing: -(-h)elp
+//
+if (showHelp)
 {
-    ImageFrame frame = capture.GrabFrame();
-    frame.GetBitmap().Save("webcam.png");
+    Helpers.PrintHelp(options);
+    return;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Default (No/Many Arguments)
+//
+ImageFrame frame = Webcam.CaptureFrame();
+frame.GetBitmap().Save("webcam.png");
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Argument Processing: -(-g)rey
+//
+if (outputGreyscale)
+{
     frame.ApplyFilter(FilterType.GREYSCALE);
     frame.GetBitmap().Save("webcam_greyscale.png");
+    frame.Reset();
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Argument Processing: --histogram
+//
+if (outputGreyscaleHistogram)
+{
     frame.GenerateGreyscaleHistogram().Save("webcam_greyscale_histogram.png");
 }
-void TakeWebcamImageAll()
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Argument Processing: -(-b)lur
+//
+if (outputGaussianBlur)
 {
-    ImageFrame frame = capture.GrabFrame();
-    frame.GetBitmap().Save("webcam.png");
-    frame.ApplyFilter(FilterType.GREYSCALE);
-    frame.GetBitmap().Save("webcam_greyscale.png");
-    // Do the others
+    frame.ApplyFilter(FilterType.GAUSSIAN_BLUR, (kSizeX, kSizeY), sigmaX);
+    frame.GetBitmap().Save("webcam_gaussian_blur.png");
+    frame.Reset();
 }
 
-bool continueLoop = true;
-while (continueLoop) {
-    // Input prompt
-    Console.WriteLine("+===============================+\r\n" +
-        "| IMAGE PROCESSING CLI (WEBCAM) |\r\n" +
-        "+===============================+\r\n\r\n" +
-        "1. Take normal image\r\n" +
-        "2. Take normal image, apply greyscale filters (plus histogram)\r\n" +
-        "3. Take normal image, apply all filters\r\n" +
-        "4. Exit\r\n");
-
-    char key = Console.ReadKey().KeyChar;
-    switch (key)
-    {
-        case '1':
-            TakeWebcamImage();
-            break;
-
-        case '2':
-            TakeWebcamImageGreyscale();
-            break;
-
-        case '3':
-            TakeWebcamImageAll();
-            break;
-
-        case '4':
-            continueLoop = false;
-            break;
-    }
-
-    Console.Clear();
+///////////////////////////////////////////////////////////////////////////////
+//
+// Argument Processing: -(-e)dgedetection
+//
+if (outputCannyEdgeDetection)
+{
+    frame.ApplyFilter(FilterType.CANNY_EDGE_DETECTION, (thresholdLower, thresholdUpper));
+    frame.GetBitmap().Save("webcam_canny_edge_detection.png");
+    frame.Reset();
 }
