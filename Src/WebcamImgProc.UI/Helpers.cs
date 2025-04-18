56f8c199ce91cc9bc2ed3bd2c530bb789ab08b1e
officialSaqib <143649215+officialSaqib@users.noreply.github.com>
2025-04-18 01:29:54 -0400

    Implemented a basic-working non-MVVM WPF UI

---
+++ Src/WebcamImgProc.UI/Helpers.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace WebcamImgProc.UI
+{
+    /// <summary>
+    /// Generic helpers that the WPF form can use.
+    /// </summary>
+    internal class Helpers
+    {
+        /// <summary>
+        /// Takes a bitmap and converts it to an image that can be handled by
+        /// WPF ImageBrush.
+        /// </summary>
+        /// <remarks>
+        /// Source: https://stackoverflow.com/a/34590774
+        /// </remarks>
+        /// <param name="src">A bitmap image</param>
+        /// <returns>The image as a BitmapImage for WPF.</returns>
+        public static BitmapImage Convert(Bitmap src)
+        {
+            var ms = new MemoryStream();
+            ((Bitmap)src).Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            var image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+            return image;
+        }
+    }
+}
--- Src/WebcamImgProc.UI/MainWindow.xaml.cs
+++ Src/WebcamImgProc.UI/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Drawing;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+using WebcamImgProc.ImgProc.Capture;
+using WebcamImgProc.ImgProc.Frame;
 
 namespace WebcamImgProc.UI
 {
@@ -16,9 +21,95 @@ namespace WebcamImgProc.UI
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Main webcam which is running to capture frames.
+        /// </summary>
+        private Webcam webcam = new Webcam();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // A timer that ticks once per nanosecond, updates the view
+            var dispatchTimer = new DispatcherTimer();
+            dispatchTimer.Tick += new EventHandler(TimerTickEvent);
+            dispatchTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            dispatchTimer.Start();
+
+            // WPF sliders have a bug (I think?) where it calls the event if I
+            // set the minimum in the properties, before the window is even
+            // initialized. Could probably be avoided if I had a good MVVM
+            // system instead
+            this.KernelXSlider.Minimum = 1;
+            this.KernelYSlider.Minimum = 1;
+            this.SigmaXSlider.Minimum = 1;
+            this.ThresholdLowerSlider.Minimum = 1;
+            this.ThresholdUpperSlider.Minimum = 1;
+        }
+
+        private void TimerTickEvent(object sender, EventArgs e)
+        {
+            if (this.PauseFrameCheckBox.IsChecked == true)
+                return;
+
+            ImageFrame fr = webcam.GrabFrame();
+            switch (this.FilterTypeComboBox.SelectedIndex)
+            {
+                case 1:
+                    fr.ApplyFilter(FilterType.GREYSCALE);
+                    break;
+
+                case 2:
+                    fr.ApplyFilter(FilterType.GAUSSIAN_BLUR,
+                        ((int)this.KernelXSlider.Value, (int)this.KernelYSlider.Value),
+                        this.SigmaXSlider.Value);
+                    break;
+
+                case 3:
+                    fr.ApplyFilter(FilterType.CANNY_EDGE_DETECTION,
+                        (this.ThresholdLowerSlider.Value, this.ThresholdUpperSlider.Value));
+                    break;
+
+                default:
+                    break;
+            }
+
+            // Setting source for each WPF image
+            this.WebcamFrameImage.Source = Helpers.Convert(fr.GetBitmap());
+            this.WebcamFrameImageGreyscale.Source = Helpers.Convert(fr.GenerateGreyscaleHistogram());
+        }
+
+        private void KernelXSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.KernelXValueLabel.Content = $"({e.NewValue})";
+        }
+        private void KernelYSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.KernelYValueLabel.Content = $"({e.NewValue})";
+        }
+        private void SigmaXSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.SigmaXValueLabel.Content = $"({e.NewValue})";
+        }
+        private void ThresholdLowerSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (this.ThresholdUpperSlider.Value < e.NewValue)
+                this.ThresholdUpperSlider.Value = e.NewValue;
+
+            this.ThresholdLowerValueLabel.Content = $"({e.NewValue*10})";
+        }
+        private void ThresholdUpperSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (this.ThresholdLowerSlider.Value > e.NewValue)
+                this.ThresholdLowerSlider.Value = e.NewValue;
+
+            this.ThresholdUpperValueLabel.Content = $"({e.NewValue*10})";
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // This is important!
+            this.webcam.Release();
         }
     }
 }
